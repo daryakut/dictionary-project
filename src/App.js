cb68d7cebbf8427eaeb7e1acf3550a66f683ea6d
daria kutluieva <daryakutlueva@gmail.com>
2022-07-26 00:38:49 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import logo from "./logo.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo"></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
