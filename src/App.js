fc1064ad65d2489a9e1b0008bb477c3dd87256de
daria kutluieva <daryakutlueva@gmail.com>
2022-07-26 15:13:01 +0200

    added api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import logo from "./logo.jpg";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
@@ -7,6 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo"></img>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Daria</small>
+      </footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+import axios from "axios";
+
+export default function Dictionary(params) {
+  let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleKeywordChange(event) {
+    console.log(event.target.value);
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
